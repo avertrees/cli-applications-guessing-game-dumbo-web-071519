d02aac90369855d6d4a506fd868efd573f93b53e
Alessandra Vertrees <avertrees447@gmail.com>
2019-07-09 16:32:02 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -14,7 +14,7 @@ def run_guessing_game
  
   input = gets.chomp
   
-  if input == number
+  if number == input
     puts "You guessed the correct number!"
   elsif input == "exit"
     puts "Goodbye!"
