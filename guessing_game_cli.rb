e56edebbf75e1f80a76dcecc6e05114e88f85c21
Alessandra Vertrees <avertrees447@gmail.com>
2019-07-09 16:10:18 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,18 @@
 # Code your solution here!
+def generate_number 
+  return rand(6) + 1
+end
+
+def promp_user
+  puts "Guess a number between 1 and 6"
+end
+
+def get_user_input
+  gets.chomp
+end
+
+def compare_numbers(num1, num2)
+  if num1 == num2
+    puts "You guessed the correct number!"
+  else 
+end
